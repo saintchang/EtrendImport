306d291cd99532802f415b5f744bc6a8cb69ce8b
saint0612 <saint0612@hotmail.com>
2015-07-10 20:43:35 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace EtrendImport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.btnOpenSource = new System.Windows.Forms.Button();
+            this.btnDBF = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnTrans = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstError = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFrglbr = new System.Windows.Forms.TextBox();
+            this.btnFrglbr = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboCust = new System.Windows.Forms.ComboBox();
+            this.cboFrglbr = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtMapping = new System.Windows.Forms.TextBox();
+            this.btnMapping = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "客戶檔案來源(EXCEL)";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSource.Location = new System.Drawing.Point(9, 27);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(400, 27);
+            this.txtSource.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "目的地(DATA)";
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTarget.Location = new System.Drawing.Point(9, 197);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(603, 27);
+            this.txtTarget.TabIndex = 3;
+            this.txtTarget.Text = "D:\\ForBox2\\Etrend\\FManager\\DATA_0421\\people.dbc";
+            // 
+            // btnOpenSource
+            // 
+            this.btnOpenSource.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenSource.Location = new System.Drawing.Point(415, 27);
+            this.btnOpenSource.Name = "btnOpenSource";
+            this.btnOpenSource.Size = new System.Drawing.Size(36, 25);
+            this.btnOpenSource.TabIndex = 4;
+            this.btnOpenSource.Text = "...";
+            this.btnOpenSource.UseVisualStyleBackColor = true;
+            this.btnOpenSource.Click += new System.EventHandler(this.btnOpenSource_Click);
+            // 
+            // btnDBF
+            // 
+            this.btnDBF.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDBF.Location = new System.Drawing.Point(621, 197);
+            this.btnDBF.Name = "btnDBF";
+            this.btnDBF.Size = new System.Drawing.Size(36, 25);
+            this.btnDBF.TabIndex = 5;
+            this.btnDBF.Text = "...";
+            this.btnDBF.UseVisualStyleBackColor = true;
+            this.btnDBF.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(556, 332);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(101, 43);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnTrans
+            // 
+            this.btnTrans.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrans.Location = new System.Drawing.Point(449, 332);
+            this.btnTrans.Name = "btnTrans";
+            this.btnTrans.Size = new System.Drawing.Size(101, 43);
+            this.btnTrans.TabIndex = 7;
+            this.btnTrans.Text = "轉檔";
+            this.btnTrans.UseVisualStyleBackColor = true;
+            this.btnTrans.Click += new System.EventHandler(this.btnTransfer);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(453, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "選擇要匯入客戶工作表";
+            // 
+            // lstError
+            // 
+            this.lstError.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstError.FormattingEnabled = true;
+            this.lstError.ItemHeight = 19;
+            this.lstError.Location = new System.Drawing.Point(9, 257);
+            this.lstError.Name = "lstError";
+            this.lstError.Size = new System.Drawing.Size(434, 118);
+            this.lstError.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 19);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "錯誤";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(8, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 19);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "外勞檔案來源(EXCEL)";
+            // 
+            // txtFrglbr
+            // 
+            this.txtFrglbr.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFrglbr.Location = new System.Drawing.Point(9, 81);
+            this.txtFrglbr.Name = "txtFrglbr";
+            this.txtFrglbr.Size = new System.Drawing.Size(400, 27);
+            this.txtFrglbr.TabIndex = 16;
+            // 
+            // btnFrglbr
+            // 
+            this.btnFrglbr.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFrglbr.Location = new System.Drawing.Point(415, 83);
+            this.btnFrglbr.Name = "btnFrglbr";
+            this.btnFrglbr.Size = new System.Drawing.Size(36, 25);
+            this.btnFrglbr.TabIndex = 17;
+            this.btnFrglbr.Text = "...";
+            this.btnFrglbr.UseVisualStyleBackColor = true;
+            this.btnFrglbr.Click += new System.EventHandler(this.btnFrglbr_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(457, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(169, 19);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "選擇要匯入外勞工作表";
+            // 
+            // cboCust
+            // 
+            this.cboCust.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboCust.FormattingEnabled = true;
+            this.cboCust.Location = new System.Drawing.Point(457, 27);
+            this.cboCust.Name = "cboCust";
+            this.cboCust.Size = new System.Drawing.Size(200, 27);
+            this.cboCust.TabIndex = 19;
+            // 
+            // cboFrglbr
+            // 
+            this.cboFrglbr.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboFrglbr.FormattingEnabled = true;
+            this.cboFrglbr.Location = new System.Drawing.Point(457, 83);
+            this.cboFrglbr.Name = "cboFrglbr";
+            this.cboFrglbr.Size = new System.Drawing.Size(200, 27);
+            this.cboFrglbr.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(8, 120);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(181, 19);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Etrend 欄位來源(EXCEL)";
+            // 
+            // txtMapping
+            // 
+            this.txtMapping.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMapping.Location = new System.Drawing.Point(9, 142);
+            this.txtMapping.Name = "txtMapping";
+            this.txtMapping.Size = new System.Drawing.Size(606, 27);
+            this.txtMapping.TabIndex = 26;
+            this.txtMapping.Text = "D:\\ForBox2\\To Saint\\20150629\\EtrendMapping.xls";
+            // 
+            // btnMapping
+            // 
+            this.btnMapping.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMapping.Location = new System.Drawing.Point(621, 142);
+            this.btnMapping.Name = "btnMapping";
+            this.btnMapping.Size = new System.Drawing.Size(36, 25);
+            this.btnMapping.TabIndex = 27;
+            this.btnMapping.Text = "...";
+            this.btnMapping.UseVisualStyleBackColor = true;
+            this.btnMapping.Click += new System.EventHandler(this.btnMapping_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 386);
+            this.Controls.Add(this.btnMapping);
+            this.Controls.Add(this.txtMapping);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cboFrglbr);
+            this.Controls.Add(this.cboCust);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnFrglbr);
+            this.Controls.Add(this.txtFrglbr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstError);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnTrans);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDBF);
+            this.Controls.Add(this.btnOpenSource);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Etrend Import";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Button btnOpenSource;
+        private System.Windows.Forms.Button btnDBF;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnTrans;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstError;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFrglbr;
+        private System.Windows.Forms.Button btnFrglbr;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboCust;
+        private System.Windows.Forms.ComboBox cboFrglbr;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtMapping;
+        private System.Windows.Forms.Button btnMapping;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LinqToExcel;
+using System.Data.OleDb;
+namespace EtrendImport
+{
+    public partial class Form1 : Form
+    {
+        int cust_sn = 0; 
+        OpenFileDialog OD = new OpenFileDialog();
+        string xlsFilterString = "Excel File (*.xls)|*.xls";
+        string dbfFilterString = "DBC File (*.dbc)|*.dbc";
+        string dbfConnectionString = "Provider=VFPOLEDB.1;Data Source={0};";
+        string mappingShtNane = string.Empty;
+        System.Data.OleDb.OleDbConnection dbfCN = null;
+        System.Data.OleDb.OleDbCommand dbfCMD = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected void btnTransfer(object sender, EventArgs e)
+        {
+            lstError.Items.Clear();
+            importToEntrend();
+        }
+
+        protected void importToEntrend()
+        {
+            if (!myValidate())
+                return;
+            string[] mappingList = {"CustMapping","FrglbrMapping" };
+            string[] sourceList = { txtSource.Text, txtFrglbr.Text };
+            string[] sourceShtList = { cboCust.SelectedItem.ToString(), cboFrglbr.SelectedItem.ToString() };
+            string[] dbfNameList = { "cust", "frglbr" };
+            string[] frgchkValues = { "", "", "", "" };
+            string sql;
+
+            dbfCN = new System.Data.OleDb.OleDbConnection(
+                string.Format(dbfConnectionString, txtTarget.Text));
+            dbfCN.Open();
+            dbfCMD = dbfCN.CreateCommand();
+            //clearDBF(dbfNameList[i]);
+            clearDBF(dbfNameList);
+
+            var mapExcel = new ExcelQueryFactory(txtMapping.Text);
+            var mappingFrgchkSht = from c in mapExcel.Worksheet<mapping>("FrgchkMapping")
+                                   where !string.IsNullOrEmpty(c.new_name)
+                                   select c;
+            StringBuilder fieldSB = new StringBuilder();
+            StringBuilder valueSB = new StringBuilder();
+            StringBuilder frgchkSB = new StringBuilder();
+            for (int i = 0; i < mappingList.Length; i++)
+            {
+                var excel = new ExcelQueryFactory(sourceList[i]);
+                var sourceSht = from s in excel.Worksheet(sourceShtList[i])
+                                select s;
+
+                var mappingSht = from c in mapExcel.Worksheet<mapping>(mappingList[i])
+                                 where !string.IsNullOrEmpty(c.new_name)
+                                 select c;
+                cust_sn = 0;
+                //dbfCMD = dbfCN.CreateCommand();
+                //clearDBF(dbfNameList[i]);
+                foreach (var itemRow in sourceSht)
+                {
+                    if (string.IsNullOrEmpty(itemRow[0].ToString()))
+                        break;
+                    cust_sn++;
+                    #region foreach (mapping mpRow in mappingSht)
+                    foreach (mapping mpRow in mappingSht)
+                    {
+                        fieldSB.AppendFormat(",{0}", mpRow.e_name);
+                        valueSB.AppendFormat(",{0}", getNewValue(mpRow, itemRow));
+                    }
+                    sql = string.Format(@"INSERT INTO {0}({1}) VALUES({2})", dbfNameList[i]
+                        , fieldSB.ToString().Substring(1)
+                        , valueSB.ToString().Substring(1));
+                    fieldSB.Clear();
+                    valueSB.Clear();
+                    try
+                    {
+                        dbfCMD.CommandType = CommandType.Text;
+                        dbfCMD.CommandText = sql;
+                        dbfCMD.ExecuteNonQuery();
+                        Application.DoEvents();
+                        //新增frgchk
+                        if (mappingList[i] == "FrglbrMapping")
+                        {
+                           
+                            frgchkSB.Clear();
+                            frgchkSB.AppendFormat(",{0}", itemRow[0].ToString());
+                            foreach (mapping mpRow in mappingFrgchkSht)
+                            {
+                                frgchkSB.AppendFormat(",{0}", getNewValue(mpRow, itemRow));
+                            }                                                        
+                            insertIntoFrgchk(
+                                frgchkSB.ToString().Substring(1).Split(new char[]{','}));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //System.Diagnostics.Debugger.Break();
+                        lstError.Items.Add(string.Format("{0}:{1}", itemRow[0].ToString(),ex.Message));
+                    }
+                    #endregion
+                }
+            }
+            dbfCN.Close();
+            dbfCN.Dispose();
+            MessageBox.Show("完成");
+        }
+
+        protected void insertIntoFrgchk(string[] p_values)
+        {
+            string sql =mapping.GetFRGCHKSql(p_values);
+            dbfCMD.Parameters.Clear();
+            dbfCMD.CommandType = CommandType.StoredProcedure;
+            dbfCMD.CommandText = "ExecScript";
+            dbfCMD.Parameters.Add("frgchkScript"
+                , OleDbType.Char).Value = sql;
+            dbfCMD.ExecuteNonQuery();
+        }
+
+        protected void clearDBF(string[] p_dbf)
+        //protected void clearDBF(string p_dbf)
+        {
+            foreach (string dbf in p_dbf)
+            {
+                string sql = string.Format(@"set exclusive on
+                                    delete from {0}
+                                    pack ", dbf);
+                dbfCMD.CommandType = CommandType.StoredProcedure;
+                dbfCMD.CommandText = "ExecScript";
+                dbfCMD.Parameters.Add("myScript", OleDbType.Char).Value = sql;
+                dbfCMD.ExecuteNonQuery();
+                dbfCMD.Parameters.Clear();
+            }
+        }
+
+        protected string getNewValue(mapping p_mapp,LinqToExcel.Row p_row)
+        {
+            string new_value = string.Empty;
+            if (!p_mapp.new_name.Equals("DEFAULT"))
+            {
+                switch (p_mapp.new_name)
+                {
+                    case "性別":
+                        new_value = p_row[p_mapp.new_name] == "女" ? "'F'" : "'M'";
+                        break;
+                    case "生日":
+                    case "入境日":
+                    case "護照期限":
+                    case "聘可期限":
+                    case "居留期限":
+                        if (p_row[p_mapp.new_name].Value == DBNull.Value)
+                        {
+                            new_value = "{^1900-01-01}";
+                        }
+                        else
+                        {
+                            new_value = "{^" + Convert.ToDateTime(p_row[p_mapp.new_name]).ToShortDateString() + "}";
+                        }                        
+                        break;
+                    case "外勞狀態":
+                        new_value = "1";
+                        break;
+                    default:
+                        new_value = string.Format("'{0}'"
+                            , p_row[p_mapp.new_name] == null ? string.Empty : p_row[p_mapp.new_name]);
+                        break;
+                }
+            }
+            else
+            {
+                if (p_mapp.e_name.Equals("CUST_SN") || p_mapp.e_name.Equals("LBR_SN"))
+                {
+                    new_value = cust_sn.ToString();                
+                }
+                else
+                {
+                    switch (p_mapp.field_type.ToUpper())
+                    {
+                        case "L":
+                            new_value = ".t.";
+                            break;
+                        case "C":
+                            new_value = "''";
+                            break;
+                        case "D":
+                        case "T":
+                            new_value = "{^1900-01-01}";
+                            break;
+                        case "I":
+                        case "N":
+                        case "Y":
+                            new_value = "0";
+                            break;
+                    }                
+                }
+            }
+            return new_value;
+        }
+
+        protected void myOpenFileDialog(TextBox p_textBox, string p_filter)
+        {
+            setOpenFileDialog(p_filter);
+            if (OD.ShowDialog() == DialogResult.OK)
+            {
+                p_textBox.Text = OD.FileName;
+            }
+        }
+
+        protected void getExclSheets(TextBox p_textBox,ComboBox p_cbo)
+        {
+            if (string.IsNullOrEmpty(p_textBox.Text))
+                return;
+            var excel = new ExcelQueryFactory(p_textBox.Text);
+            IEnumerable<string> sheets = excel.GetWorksheetNames();
+            foreach (string sht in sheets)
+            {
+                p_cbo.Items.Add(sht);
+            }
+        }
+
+        private void btnOpenSource_Click(object sender, EventArgs e)
+        {
+            myOpenFileDialog(txtSource,xlsFilterString);
+            getExclSheets(txtSource, cboCust);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myOpenFileDialog(txtTarget,dbfFilterString);
+        }
+
+        protected void setOpenFileDialog(string p_filter)
+        {
+            OD.InitialDirectory = "c:\\";
+            OD.Filter = p_filter;
+            OD.FilterIndex = 2;
+            OD.RestoreDirectory = true;
+        }
+
+        private void btnFrglbr_Click(object sender, EventArgs e)
+        {
+            myOpenFileDialog(txtFrglbr, xlsFilterString);
+            getExclSheets(txtFrglbr, cboFrglbr);
+        }
+
+        private void btnMapping_Click(object sender, EventArgs e)
+        {
+            myOpenFileDialog(txtMapping, xlsFilterString);
+        }
+
+        protected bool myValidate()
+        {
+            bool ok = true;
+            Control[] txtCollect = {txtSource,txtFrglbr,txtMapping,txtTarget};
+            //檢查是否有選擇來源檔
+            foreach (TextBox tb in txtCollect)
+            {
+                if (string.IsNullOrEmpty(tb.Text))
+                {
+                    ok = false;
+                    MessageBox.Show("請碓認是否已選擇檔案來源");
+                    tb.Focus();
+                    break;
+                }                
+            }
+            if (ok.Equals(true))
+            {
+                Control[] cboCollect = { cboCust, cboFrglbr };
+                foreach (ComboBox cb in cboCollect)
+                {
+                    if (cb.SelectedItem == null)
+                    {
+                        ok = false;
+                        MessageBox.Show("請碓認是否已選擇工作表");
+                        cb.Focus();
+                        break;
+                    }
+                }            
+            }
+            return ok;
+        }
+    }
+}
